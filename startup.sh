8622946133d85e43865d69a8d6b25f7ddffd8615
Herman <herman@dompseler.nl>
2019-05-16 14:10:46 +0200

    create database

---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+# ### CONFIGURE DOCTRINE ###
+
+# get admin password
+MY_PASSWORD=admin
+if [ -f "$DB_PASS_FILE" ]
+then
+	MY_PASSWORD=`cat $DB_PASS_FILE`
+fi
+
+# set password
+export DB_PASS=$MY_PASSWORD
+
+# create schema
+cd /data
+bin/console doctrine:schema:create
+
+exec "$@"
